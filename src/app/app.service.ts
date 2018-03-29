6637f42324f29e3403424448e9cbd06657d375c4
Hervinder Singh <hervinder1410@gmail.com>
2018-03-29 02:27:56 -0700

    ok

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,12 +10,12 @@ export class RootService{
    constructor(private _http:Http){}
    
    addSingupDetails(info){
-          return this._http.post("http://ileadcorporation.com/beayou_test/signup/signup_company.php",info)
+          return this._http.post("https://beayou.in/signup/signup_company.php",info)
           .map((response) => response.json());
    }
    login_auth(info){
        if(info['member'] === 'company'){
-        return this._http.post("http://ileadcorporation.com/beayou_test/signin/signin_company.php",info)
+        return this._http.post("https://beayou.in/signin/signin_company.php",info)
         .map((response) => {
           let res=  response.json();
           console.log(res);     
@@ -23,7 +23,7 @@ export class RootService{
         });
        }
        else if(info['member'] === 'talent'){
-        return this._http.post("http://ileadcorporation.com/beayou_test/signin/signin_talent.php",info)
+        return this._http.post("https://beayou.in/signin/signin_talent.php",info)
         .map((response) => response.json());
        }
        else if(info['member'] === 'training'){
@@ -47,11 +47,11 @@ export class RootService{
       }
    }
    addTalentDetails(info){
-    return this._http.post("http://ileadcorporation.com/beayou_test/signup/signup_talent.php",info)
+    return this._http.post("https://beayou.in/signup/signup_talent.php",info)
     .map((response) => response.json());
    }
    addTraineeDetails(info){
-    return this._http.post("http://ileadcorporation.com/beayou_test/signup/signup_training.php",info)
+    return this._http.post("https://beayou.in/signup/signup_training.php",info)
     .map((response) => response.json());
    }
 }
